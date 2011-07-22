ac9aef8c1856cd23c8edc970f90cd63e9ff9d82b
Michal Novotny <minovotn@redhat.com>
2011-07-22 15:21:27 +0200

    Update to version 0.4.3
    Signed-off-by: Michal Novotny <minovotn@redhat.com>

--- src/libvirt-php.h
+++ src/libvirt-php.h
@@ -108,7 +108,7 @@ ZEND_END_MODULE_GLOBALS(libvirt)
 #define LIBVIRT_G(v) (libvirt_globals.v)
 #endif
 
-#define PHP_LIBVIRT_WORLD_VERSION "0.4.2"
+#define PHP_LIBVIRT_WORLD_VERSION "0.4.3"
 #define PHP_LIBVIRT_WORLD_EXTNAME "libvirt"
 
 /* Internal resource identifier objects */
