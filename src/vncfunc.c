96ab983fc5ae504fb4df45da21dd9388dbcf45c4
Michal Novotny <minovotn@redhat.com>
2011-08-19 11:39:52 +0200

    Put socket and VNC functions to separate source files
    Signed-off-by: Michal Novotny <minovotn@redhat.com>

--- src/vncfunc.c
+++ src/vncfunc.c
@@ -1,5 +1,5 @@
 /*
-* vnc.c: VNC Client functions to be used for the graphical VNC console of libvirt-php
+* vncfunc.c: VNC Client functions to be used for the graphical VNC console of libvirt-php
 *
 * See COPYING for the license of this software
 *
@@ -12,7 +12,7 @@
 #ifdef DEBUG_VNC
 #define DPRINTF(fmt, ...) \
 if (gdebug)	\
-do { fprintf(stderr, "[%s ", get_datetime()); fprintf(stderr, "libvirt-php/vnc ]: " fmt , ## __VA_ARGS__); fflush(stderr); } while (0)
+do { fprintf(stderr, "[%s ", get_datetime()); fprintf(stderr, "libvirt-php/vnc    ]: " fmt , ## __VA_ARGS__); fflush(stderr); } while (0)
 #else
 #define DPRINTF(fmt, ...) \
 do {} while(0)
@@ -40,84 +40,6 @@ typedef struct tServerFBParams {
 	int desktopNameLen;
 	unsigned char *desktopName;
 } tServerFBParams;
-
-/*
-	Private function name:	connect_socket
-	Since version:		0.4.2
-	Description:		Function is used to connect the socket and return the socket descriptor or error
-	Arguments:		@server [string]: server string to connect to
-				@port [string]: string version of port value to connect to
-				@keepalive [bool]: determines whether to create keepalive socket or not
-				@nodelay [bool]: determines whether to set no-delay option on socket
-	Returns:		socket descriptor on success, -errno otherwise
-*/
-int connect_socket(char *server, char *port, int keepalive, int nodelay)
-{
-	struct addrinfo hints;
-	struct addrinfo *result, *rp;
-	int sfd, s;
-	char name[1024] = { 0 };
-
-	memset(&hints, 0, sizeof(struct addrinfo));
-	hints.ai_family = AF_UNSPEC;
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_flags = 0;
-	hints.ai_protocol = 0;
-
-	/* Get the current hostname and override to localhost if local machine */
-	gethostname(name, 1024);
-	if (strcmp(name, server) == 0)
-		server = strdup("localhost");
-
-	DPRINTF("%s: Connecting to %s:%s\n", PHPFUNC, server, port);
-
-	s = getaddrinfo(server, port, &hints, &result);
-	if (s != 0)
-		return -errno;
-
-	for (rp = result; rp != NULL; rp = rp->ai_next) {
-		sfd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
-		if (sfd == -1)
-			continue;
-
-		if (connect(sfd, rp->ai_addr, rp->ai_addrlen) != -1)
-			break;
-
-		close(sfd);
-	}
-
-	if (rp == NULL)
-		return -errno;
-
-	freeaddrinfo(result);
-	DPRINTF("%s: Socket descriptor #%d opened\n", PHPFUNC, sfd);
-
-	if (keepalive) {
-		int on = 1;
-		if (setsockopt(sfd, SOL_SOCKET, SO_KEEPALIVE, &on, sizeof(on)) < 0) {
-			int err = errno;
-			close(sfd);
-			DPRINTF("%s: Cannot set keep alive option on socket\n", PHPFUNC);
-			return -err;
-		}
-
-		DPRINTF("%s: Socket #%d set as keepalive socket\n", PHPFUNC, sfd);
-	}
-
-	if (nodelay) {
-		int on = 1;
-		if (setsockopt(sfd, IPPROTO_TCP, TCP_NODELAY, &on, sizeof(on)) < 0) {
-			int err = errno;
-			close(sfd);
-			DPRINTF("%s: Cannot set no delay option on socket\n", PHPFUNC);
-			return -err;
-		}
-
-		DPRINTF("%s: Socket #%d set as no delay socket\n", PHPFUNC, sfd);
-	}
-
-	return sfd;
-}
 
 /*
 	Private function name:	vnc_write_client_version
@@ -437,83 +359,6 @@ int vnc_set_encoding(int sfd)
 
 	DPRINTF("%s: Client encoding set\n", PHPFUNC);	
 	return 0;
-}
-
-/*
-	Private function name:	socket_has_data
-	Since version:		0.4.3
-	Description:		Function to check socket for data in queue
-	Arguments:		@sfd [int]: socket descriptor for existing VNC client socket
-				@maxtime [bool]: maximum wait time in milliseconds
-				@ignoremsg [bool]: flag to print messages if debug log enabled (useful to set to 1 for recursion)
-	Returns:		0 on success, -errno otherwise
-*/
-int socket_has_data(int sfd, long maxtime, int ignoremsg)
-{
-	fd_set fds;
-	struct timeval timeout;
-	int rc, result;
-
-	timeout.tv_sec = maxtime / 1000000;
-	timeout.tv_usec = (maxtime % 1000000);
-
-	if (!ignoremsg)
-		DPRINTF("%s: Checking data on socket %d, timeout = { %ld, %ld }\n", PHPFUNC, sfd,
-			(long)timeout.tv_sec, (long)timeout.tv_usec);
-
-	FD_ZERO(&fds);
-	FD_SET(sfd, &fds);
-	rc = select( sizeof(fds), &fds, NULL, NULL, &timeout);
-	if (rc==-1) {
-		DPRINTF("%s: Select with error %d (%s)\n", PHPFUNC, errno, strerror(-errno));
-		return -errno;
-	}
-
-	if (!ignoremsg)
-		DPRINTF("%s: Select returned %d\n", PHPFUNC, rc);
-		
-	return (rc == 1);
-}
-
-/*
-	Private function name:	socket_read
-	Since version:		0.4.3
-	Description:		Function to read the data from socket
-	Arguments:		@sfd [int]: socket descriptor for existing VNC client socket
-				@length [bool]: length of the data to be read or -1 for all the data
-	Returns:		0 on success, -errno otherwise
-*/
-void socket_read(int sfd, long length)
-{
-	long len = 0;
-	unsigned char bigbuf[1048576];
-
-	if (socket_has_data(sfd, 50000, 0) != 1) {
-		DPRINTF("%s: No data appears to be available\n", PHPFUNC);
-		return;
-	}
-	
-	if (length == -1) {
-		DPRINTF("%s: Reading all the data from socket\n", PHPFUNC);
-		while (socket_has_data(sfd, 50000, 1) == 1)
-			while ((len = read(sfd, bigbuf, sizeof(bigbuf))) == sizeof(bigbuf)) ;
-		DPRINTF("%s: Read done ...\n", PHPFUNC);
-		return;
-	}
-
-	DPRINTF("%s: Reading %ld bytes\n", PHPFUNC, length);
-	while (length > 0) {
-		len = read(sfd, bigbuf, sizeof(bigbuf));
-
-		length -= len;
-		if (length < 0)
-			length = 0;
-	}
-
-	if (length)
-		read(sfd, bigbuf, length);
-
-	DPRINTF("%s: All bytes read\n", PHPFUNC);
 }
 
 /*
