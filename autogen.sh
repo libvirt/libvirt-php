05dd00705afcc28db42a3abb2a625cba92b4c864
Michal Privoznik <mprivozn@redhat.com>
2017-08-05 14:17:07 +0200

    Generate AUTHORS file
    This is basically taken from libvirt repo. I'm creating .mailmap
    to avoid doubled entries in the AUTHORS file. Also, AUTHORS is no
    longer tracked by git.
    
    Signed-off-by: Michal Privoznik <mprivozn@redhat.com>

--- autogen.sh
+++ autogen.sh
@@ -2,5 +2,8 @@
 test -n "$srcdir" || srcdir=`dirname "$0"`
 test -n "$srcdir" || srcdir=.
 
+# Automake requires that ChangeLog and AUTHORS exist.
+touch AUTHORS ChangeLog || exit 1
+
 autoreconf --force --install --verbose "$srcdir"
 test -n "$NOCONFIGURE" || "$srcdir/configure" "$@"
